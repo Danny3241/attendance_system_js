8f13a26522d5b634bd6b78dd0a3c8efc7f243cd6
Danny3241 <sakhiyadenish33@gmailcom>
2023-05-02 22:30:28 +0530

    first commit

---
+++ student.js
@@ -0,0 +1,59 @@
+document = document.getElementById("form1").addEventListener("submit", submitFun1);
+
+var studentDataArr =JSON.parse(localStorage.getItem("studentData"))|| [];
+function submitFun1(e) {
+    document.querySelector("#tbody").innerHTML = "";
+    e.preventDefault();
+    var name = document.querySelector("#name").value;
+    var date=document.querySelector("#date").value;
+
+    var studentObj = {
+        name: name,
+        date: date
+    }
+
+    studentDataArr.push(studentObj);
+    localStorage.setItem("studentData", JSON.stringify(studentDataArr));
+    document.querySelector("#form1").reset();
+    alert("Student Added Successfully");
+
+    displayFun(studentDataArr)
+}
+
+function displayFun(studentDataArr) {
+
+    var count = 1;
+    studentDataArr.map(function (item) {
+    
+        var tr = document.createElement("tr");
+
+        var td1 = document.createElement("td");
+        td1.innerHTML = count++
+        var td2 = document.createElement("td");
+        td2.innerHTML = item.name;
+        var td3 = document.createElement("td");
+        var btn1 = document.createElement("button");
+        btn1.innerHTML = "P";
+        btn1.addEventListener("click", function () {
+            td3.innerHTML = "<button >Present</button>";
+        });
+        var btn2 = document.createElement("button");
+        btn2.innerHTML = "A";
+        btn2.addEventListener("click", function () {
+            td3.innerHTML = "<button id='absent'>Absent</button>";
+        
+        });
+        td3.classList.add("td3");
+        td3.append(btn1, btn2);
+        var td4= document.createElement("td");
+        td4.innerHTML = item.date;
+
+        tr.append(td1, td2, td3, td4);
+
+        document.querySelector("#tbody").append(tr);
+
+    });
+
+
+}
+displayFun(studentDataArr);
